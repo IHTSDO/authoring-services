6c6d3907ce3dca8e2f6fef9846ea6b2d1a385ec7
Kai Kewley <kaikewley@gmail.com>
2017-03-03 11:52:53 +0000

    TBBA-103 Correct mime type in LOINC export response.

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/LOINCExportController.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/LOINCExportController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import net.rcarz.jiraclient.JiraException;
+import org.apache.http.entity.ContentType;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.ihtsdo.snowowl.authoring.single.api.pojo.*;
 import org.ihtsdo.snowowl.authoring.single.api.service.loinc.LOINCReferenceSetExportService;
@@ -21,6 +22,8 @@ import java.io.InputStream;
 @RequestMapping
 public class LOINCExportController {
 
+	private static final ContentType TSV_CONTENT_TYPE = ContentType.create("text/tab-separated-values", "UTF-8");
+
 	@Autowired
 	private LOINCReferenceSetExportService exportService;
 
@@ -30,6 +33,7 @@ public class LOINCExportController {
 	})
 	@RequestMapping(value="/loinc-export/{branchPath}", method=RequestMethod.GET, produces="text/tab-separated-values")
 	public void listProjects(@PathVariable String branchPath, HttpServletResponse response) throws BusinessServiceException, IOException {
+		response.setContentType(TSV_CONTENT_TYPE.toString());
 		exportService.exportDelta(branchPath, response.getOutputStream());
 	}
 
