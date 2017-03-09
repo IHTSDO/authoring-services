723af482aec826b13ee4fc1d9207abc0118ed189
Kai Kewley <kaikewley@gmail.com>
2017-03-09 15:48:41 +0000

    Not-found errors return 404, logged as debug without stack trace.

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/ControllerConfig.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/ControllerConfig.java
@@ -1,8 +1,11 @@
 package org.ihtsdo.snowowl.authoring.single.api.rest;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.ihtsdo.otf.rest.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,13 +15,43 @@ public class ControllerConfig {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@ExceptionHandler(Exception.class)
-	void exceptionCatchAll(Exception e) {
+	ResponseEntity<Error> exceptionCatchAll(Exception e) {
 		logger.error("{}", e);
+		return response(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	void catchNotFoundException(ResourceNotFoundException e) {
-		logger.debug("{}", e);
+	ResponseEntity<Error> catchNotFoundException(ResourceNotFoundException e) {
+		logger.debug("{}", e.getMessage());
+		return response(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+	private ResponseEntity<Error> response(String message, HttpStatus status) {
+		return new ResponseEntity<>(new Error(status, message), status);
+	}
+
+	@JsonPropertyOrder({"status", "statusMessage", "message"})
+	private static final class Error {
+
+		private HttpStatus status;
+		private String message;
+
+		Error(HttpStatus status, String message) {
+			this.status = status;
+			this.message = message;
+		}
+
+		public int getStatus() {
+			return status.value();
+		}
+
+		public String getStatusMessage() {
+			return status.getReasonPhrase();
+		}
+
+		public String getMessage() {
+			return message;
+		}
 	}
 
 }
