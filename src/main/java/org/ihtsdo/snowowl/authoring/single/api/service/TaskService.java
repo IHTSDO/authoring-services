IHTSDO/reporting-engine

TBBA-102 Add projectTemplatesDisabled field from Jira radio button "SCA Project Templates".

org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/TaskService.java
@@ -78,6 +78,7 @@ public class TaskService {
 	private final String jiraProjectPromotionField;
 	private final String jiraProjectMrcmField;
 	private final String jiraCrsIdField;
+	private final String jiraProjectTemplatesField;
 
 	private LoadingCache<String, ProjectDetails> projectDetailsCache;
 
@@ -94,6 +95,7 @@ public class TaskService {
 		jiraProjectPromotionField = JiraHelper.fieldIdLookup("SCA Project Promotion", jiraClientForFieldLookup);
 		jiraProjectMrcmField = JiraHelper.fieldIdLookup("SCA Project MRCM", jiraClientForFieldLookup);
 		jiraCrsIdField = JiraHelper.fieldIdLookup("CRS-ID", jiraClientForFieldLookup);
+		jiraProjectTemplatesField = JiraHelper.fieldIdLookup("SCA Project Templates", jiraClientForFieldLookup);
 
 		init();
 	}
@@ -175,6 +177,7 @@ public class TaskService {
 				final String latestClassificationJson = classificationService.getLatestClassification(branchPath);
 				final boolean promotionDisabled = "Disabled".equals(JiraHelper.toStringOrNull(magicTicket.getField(jiraProjectPromotionField)));
 				final boolean mrcmDisabled = "Disabled".equals(JiraHelper.toStringOrNull(magicTicket.getField(jiraProjectMrcmField)));
+				final boolean templatesDisabled = "Disabled".equals(JiraHelper.toStringOrNull(magicTicket.getField(jiraProjectTemplatesField)));
 
 				
 				final Branch branchOrNull = branchService.getBranchOrNull(branchPath);
@@ -189,7 +192,8 @@ public class TaskService {
 				}
 				branchPaths.add(branchPath);
 				final AuthoringProject authoringProject = new AuthoringProject(project.getKey(), project.getName(),
-						getPojoUserOrNull(project.getLead()), branchPath, branchState, latestClassificationJson, promotionDisabled, mrcmDisabled);
+						getPojoUserOrNull(project.getLead()), branchPath, branchState, latestClassificationJson,
+						promotionDisabled, mrcmDisabled, templatesDisabled);
 				authoringProject.setMetadata(metadata);
 				authoringProjects.add(authoringProject);
 			}

IHTSDO/authoring-services

src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/TaskService.java
@@ -84,6 +84,7 @@ public class TaskService {
 	private final String jiraProjectPromotionField;
 	private final String jiraProjectMrcmField;
 	private final String jiraCrsIdField;
+	private final String jiraProjectTemplatesField;
 
 	private LoadingCache<String, ProjectDetails> projectDetailsCache;
 	private final ExecutorService executorService;
@@ -101,6 +102,7 @@ public class TaskService {
 		jiraProjectPromotionField = JiraHelper.fieldIdLookup("SCA Project Promotion", jiraClientForFieldLookup);
 		jiraProjectMrcmField = JiraHelper.fieldIdLookup("SCA Project MRCM", jiraClientForFieldLookup);
 		jiraCrsIdField = JiraHelper.fieldIdLookup("CRS-ID", jiraClientForFieldLookup);
+		jiraProjectTemplatesField = JiraHelper.fieldIdLookup("SCA Project Templates", jiraClientForFieldLookup);
 		logger.info("Jira custom field names fetched. (e.g. {}).", jiraExtensionBaseField);
 		executorService = Executors.newCachedThreadPool();
 
@@ -187,6 +189,7 @@ public class TaskService {
 
 				final boolean promotionDisabled = "Disabled".equals(JiraHelper.toStringOrNull(projectTicket.getField(jiraProjectPromotionField)));
 				final boolean mrcmDisabled = "Disabled".equals(JiraHelper.toStringOrNull(projectTicket.getField(jiraProjectMrcmField)));
+				final boolean templatesDisabled = "Disabled".equals(JiraHelper.toStringOrNull(projectTicket.getField(jiraProjectTemplatesField)));
 
 				final Branch branchOrNull = branchService.getBranchOrNull(branchPath);
 				String parentPath = PathHelper.getParentPath(branchPath);
@@ -213,7 +216,7 @@ public class TaskService {
 				Map<String, JiraProject> projectMap = unfilteredProjects.get();
 				JiraProject project = projectMap.get(projectKey);
 				final AuthoringProject authoringProject = new AuthoringProject(projectKey, project.getName(),
-						project.getLead(), branchPath, branchState, latestClassificationJson, promotionDisabled, mrcmDisabled);
+						project.getLead(), branchPath, branchState, latestClassificationJson, promotionDisabled, mrcmDisabled, templatesDisabled);
 				authoringProject.setMetadata(metadata);
 				synchronized (authoringProjects) {
 					authoringProjects.add(authoringProject);