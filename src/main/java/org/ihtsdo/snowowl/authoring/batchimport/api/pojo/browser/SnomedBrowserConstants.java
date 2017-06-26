52a95fa270aebbc80b0deecec1ef28a20a31eb6c
Peter Groves Williams <petergwilliams@hotmail.com>
2017-06-26 18:28:54 +0100

    First pass at adding batch import to authoring services.  Incomplete

---
+++ src/main/java/org/ihtsdo/snowowl/authoring/batchimport/api/pojo/browser/SnomedBrowserConstants.java
@@ -0,0 +1,22 @@
+package org.ihtsdo.snowowl.authoring.batchimport.api.pojo.browser;
+
+public interface SnomedBrowserConstants {
+	
+	final String SCTID_ISA = "116680003";
+	final String SCTID_EN_GB = "900000000000508004";
+	final String SCTID_EN_US = "900000000000509007";
+	
+	public static final String EN_LANGUAGE_CODE = "en";
+
+	enum RelationshipModifier { EXISTENTIAL }
+	
+	enum CharacteristicType { STATED, INFERRED }
+	
+	enum Acceptability { ACCEPTABLE, PREFERRED }
+	
+	enum CaseSignificance { ENTIRE_TERM_CASE_SENSITIVE, CASE_INSENSITIVE, INITIAL_CHARACTER_CASE_INSENSITIVE }
+	
+	enum DescriptionType { SYNONYM, FSN }
+	
+	enum DefinitionStatus { PRIMITIVE, FULLY_DEFINED }
+}
