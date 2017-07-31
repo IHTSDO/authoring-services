64c5e561029138595227abd6dc39c2abce82c873
Kai Kewley <kaikewley@gmail.com>
2017-07-31 19:27:37 +0100

    Endpoint to export LOINC RF2 delta with existing member UUIDs.

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/LOINCExportController.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/LOINCExportController.java
@@ -5,18 +5,17 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import net.rcarz.jiraclient.JiraException;
 import org.apache.http.entity.ContentType;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.*;
 import org.ihtsdo.snowowl.authoring.single.api.service.loinc.LOINCReferenceSetExportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Api("Export LOINC Reference Set")
 @RestController
@@ -24,18 +23,38 @@ import java.io.InputStream;
 public class LOINCExportController {
 
 	private static final ContentType TSV_CONTENT_TYPE = ContentType.create("text/tab-separated-values", "UTF-8");
+	private static final SimpleDateFormat RELEASE_FILE_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
 	@Autowired
 	private LOINCReferenceSetExportService exportService;
 
-	@ApiOperation(value="Retrieve LOINC Reference Set Export")
+	@ApiOperation(value="Export LOINC Reference Set RF2 Delta without UUIDs")
 	@ApiResponses({
 			@ApiResponse(code = 200, message = "OK")
 	})
 	@RequestMapping(value="/loinc-export/{branchPath}", method=RequestMethod.GET, produces="text/tab-separated-values")
-	public void exportLOINCReferenceSet(@PathVariable String branchPath, HttpServletResponse response) throws BusinessServiceException, IOException {
+	public void exportLOINCReferenceSetWithoutUuids(@PathVariable String branchPath, HttpServletResponse response) throws BusinessServiceException, IOException {
 		response.setContentType(TSV_CONTENT_TYPE.toString());
-		exportService.exportDelta(BranchPathUriUtil.parseBranchPath(branchPath), response.getOutputStream());
+		setResponseFilename(response);
+		exportService.exportDelta(BranchPathUriUtil.parseBranchPath(branchPath), null, response.getOutputStream());
+	}
+
+	@ApiOperation(value="Export LOINC Reference Set RF2 Delta with UUIDs")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "OK")
+	})
+	@RequestMapping(value="/loinc-export/{branchPath}", method=RequestMethod.POST, consumes = "multipart/form-data", produces="text/tab-separated-values")
+	public void exportLOINCReferenceSetWithUuids(@PathVariable String branchPath,
+												 @RequestParam MultipartFile previousLoincRF2SnapshotFile,
+												 HttpServletResponse response) throws BusinessServiceException, IOException {
+		response.setContentType(TSV_CONTENT_TYPE.toString());
+		setResponseFilename(response);
+		exportService.exportDelta(BranchPathUriUtil.parseBranchPath(branchPath), previousLoincRF2SnapshotFile.getInputStream(), response.getOutputStream());
+	}
+
+	private void setResponseFilename(HttpServletResponse response) {
+		response.setHeader("Content-Disposition",
+				"attachment; filename=\"der2_sRefset_LOINCSimpleMapDelta_INT_" + RELEASE_FILE_DATE_FORMAT.format(new Date()) + ".txt\"");
 	}
 
 }
