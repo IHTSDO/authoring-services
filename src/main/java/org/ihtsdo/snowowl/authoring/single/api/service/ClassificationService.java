81d54010dfd3062e692b103f934bc3be9f31b9ee
Kai Kewley <kaikewley@gmail.com>
2018-05-09 17:32:12 +0100

    APDS-599 Remove classification retry logic.
    Snow Owl can now do concurrent exports so this logic needs to be removed.

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/ClassificationService.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/ClassificationService.java
@@ -53,23 +53,19 @@ public class ClassificationService {
 
 		//Now start an asynchronous thread to wait for the results
 		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		(new Thread(new ClassificationPoller(branchPath, projectKey, taskKey, results, authentication))).start();
+		(new Thread(new ClassificationPoller(projectKey, taskKey, results, authentication))).start();
 
 		return new Classification(results);
 	}
 
 	private class ClassificationPoller implements Runnable {
 
-		private Date startDate;
-		private final String branchPath;
 		private ClassificationResults results;
 		private String projectKey;
 		private String taskKey;
 		private final Authentication authentication;
 
-		ClassificationPoller(String branchPath, String projectKey, String taskKey, ClassificationResults results, Authentication authentication) {
-			this.startDate = new Date();
-			this.branchPath = branchPath;
+		ClassificationPoller(String projectKey, String taskKey, ClassificationResults results, Authentication authentication) {
 			this.results = results;
 			this.projectKey = projectKey;
 			this.taskKey = taskKey;
@@ -82,26 +78,10 @@ public class ClassificationService {
 			String resultMessage = null;
 			SnowOwlRestClient terminologyServerClient = snowOwlRestClientFactory.getClient();
 			try {
-
-				try {
-					// Function sleeps here
-					// - Throws RestClientException if classification failed
-					terminologyServerClient.waitForClassificationToComplete(results);
-				} catch (RestClientException e) {
-					if (terminologyServerClient.isUseExternalClassificationService() && startDate.after(getTimeSecondsInPast(21))) {
-						// Retry classification just once
-						logger.info("Classification failed on {} in the first {} seconds. Probably due to lack of concurrent RF2 export support in Snow Owl. " +
-								"Waiting 30 seconds before retrying once.", branchPath, 20);
-						Thread.sleep(30_000);
-						results = terminologyServerClient.startClassification(branchPath);
-						terminologyServerClient.waitForClassificationToComplete(results);
-					} else {
-						// Let exception bubble up
-						throw e;
-					}
-				}
+				// Function sleeps here
+				// - Throws RestClientException if classification failed
+				terminologyServerClient.waitForClassificationToComplete(results);
 				resultMessage = "Classification completed successfully";
-
 			} catch (RestClientException | InterruptedException e) {
 				resultMessage = "Classification failed to complete due to an internal error. Please try again.";
 				logger.error(resultMessage, e);
@@ -115,12 +95,6 @@ public class ClassificationService {
 				taskService.addCommentLogErrors(projectKey, resultMessage);
 			}
 			notificationService.queueNotification(ControllerHelper.getUsername(), new Notification(projectKey, taskKey, EntityType.Classification, resultMessage));
-		}
-
-		private Date getTimeSecondsInPast(int seconds) {
-			GregorianCalendar calendar = new GregorianCalendar();
-			calendar.add(Calendar.SECOND, -seconds);
-			return calendar.getTime();
 		}
 
 	}
