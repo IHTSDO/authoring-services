0a2cf51f43928b4bc445a2f21657dedd5f1488f3
Quyen Ly <quyen.ly@huntermacdonald.com>
2018-05-15 09:12:07 +0700

    MAINT-36 Add debug log

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/ConfigurationService.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/ConfigurationService.java
@@ -4,6 +4,8 @@ import java.net.URISyntaxException;
 
 import org.ihtsdo.snowowl.authoring.single.api.service.jira.ImpersonatingJiraClientFactory;
 import org.ihtsdo.snowowl.authoring.single.api.service.jira.JiraHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.rcarz.jiraclient.JiraClient;
 import net.rcarz.jiraclient.JiraException;
@@ -13,11 +15,13 @@ public class ConfigurationService {
 	private final String  jiraUser;
 	private final String groupName;
 	private static final String DEFAULT_GROUP_NAME = "ihtsdo-sca-author";
-
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	public ConfigurationService(ImpersonatingJiraClientFactory jiraClientFactory, String jiraUser, String groupName) throws JiraException {
 		this.jiraClientFactory = jiraClientFactory;
 		this.jiraUser = jiraUser;
 		
+		logger.info("Group name: " + groupName);
 		if(groupName == null || groupName.isEmpty()) {
 			this.groupName = DEFAULT_GROUP_NAME;
 		} else {
