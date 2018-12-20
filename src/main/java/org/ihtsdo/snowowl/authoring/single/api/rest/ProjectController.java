43ba39f7f0f39d2c4c31f5909afa28fa97f6864e
Kai Kewley <kaikewley@gmail.com>
2018-12-20 16:08:53 +0000

    INFRA-3107 Simplify annotations and remove dead code.

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/ProjectController.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/ProjectController.java
@@ -1,35 +1,19 @@
 package org.ihtsdo.snowowl.authoring.single.api.rest;
 
-import java.util.List;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import net.rcarz.jiraclient.JiraException;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.AuthoringMain;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.AuthoringProject;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.AuthoringTask;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.AuthoringTaskCreateRequest;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.AuthoringTaskUpdateRequest;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.MergeRequest;
-import org.ihtsdo.snowowl.authoring.single.api.pojo.ProcessStatus;
-import org.ihtsdo.snowowl.authoring.single.api.service.PromotionService;
-import org.ihtsdo.snowowl.authoring.single.api.service.RebaseService;
-import org.ihtsdo.snowowl.authoring.single.api.service.TaskAttachment;
-import org.ihtsdo.snowowl.authoring.single.api.service.TaskService;
+import org.ihtsdo.snowowl.authoring.single.api.pojo.*;
+import org.ihtsdo.snowowl.authoring.single.api.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import net.rcarz.jiraclient.JiraException;
+import java.util.List;
 
 @Api("Authoring Projects")
 @RestController
@@ -49,29 +33,23 @@ public class ProjectController {
 	private ScheduledRebaseService scheduledRebaseService;
 
 	@ApiOperation(value="List authoring Projects")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects", method= RequestMethod.GET)
 	public List<AuthoringProject> listProjects() throws JiraException, BusinessServiceException {
 		return taskService.listProjects();
 	}
 
 	@ApiOperation(value="Retrieve an authoring Project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}", method= RequestMethod.GET)
 	public AuthoringProject retrieveProject(@PathVariable final String projectKey) throws BusinessServiceException {
 		return taskService.retrieveProject(projectKey);
 	}
 
 	@ApiOperation(value="Rebase an authoring Project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/rebase", method= RequestMethod.POST)
-	public ResponseEntity<String> rebaseProject(@PathVariable final String projectKey) throws BusinessServiceException {
+	public ResponseEntity<String> rebaseProject(@PathVariable final String projectKey) {
 		ProcessStatus  processStatus = rebaseService.getProjectRebaseStatus(projectKey);
 		if (processStatus == null || !processStatus.getStatus().equals("Rebasing")) {
 			rebaseService.doProjectRebase(projectKey);
@@ -80,134 +58,104 @@ public class ProjectController {
 	}
 	
 	@ApiOperation(value="Get rebase status of an authoring project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/rebase/status", method= RequestMethod.GET)
-	public ProcessStatus getProjectRebaseStatus(@PathVariable final String projectKey) throws BusinessServiceException {
+	public ProcessStatus getProjectRebaseStatus(@PathVariable final String projectKey) {
 		return rebaseService.getProjectRebaseStatus(projectKey);
 	}
 
 	@ApiOperation(value="Promote an authoring Project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/promote", method= RequestMethod.POST)
-	public ResponseEntity<String> promoteProject(@PathVariable final String projectKey, @RequestBody MergeRequest mergeRequest) throws BusinessServiceException {
+	public ResponseEntity<String> promoteProject(@PathVariable final String projectKey, @RequestBody MergeRequest mergeRequest) {
 		ProcessStatus  processStatus = promotionService.getProjectPromotionStatus(projectKey);
 		if (processStatus == null || !processStatus.getStatus().equals("Rebasing")) {
 			promotionService.doProjectPromotion(projectKey, mergeRequest);
 		}		
 		return new ResponseEntity<>(HttpStatus.OK);
-		/*String projectBranchPath = taskService.getProjectBranchPathUsingCache(projectKey);
-		Merge merge = branchService.mergeBranchSync(projectBranchPath, PathHelper.getParentPath(projectBranchPath), mergeRequest.getSourceReviewId());
-		if (merge.getStatus() == Merge.Status.COMPLETED) {
-			List<Issue> promotedIssues = taskService.getTaskIssues(projectKey, TaskStatus.PROMOTED);
-			taskService.stateTransition(promotedIssues, TaskStatus.COMPLETED);
-			notificationService.queueNotification(ControllerHelper.getUsername(),
-					new Notification(projectKey, null, EntityType.Promotion, "Project successfully promoted"));
-		}
-		return getResponseEntity(merge);*/
 	}
 	
 	@ApiOperation(value="Retrieve status information about project promotion")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/promote/status", method= RequestMethod.GET)
-	public ProcessStatus getProjectPromotionStatus(@PathVariable final String projectKey) throws BusinessServiceException {
+	public ProcessStatus getProjectPromotionStatus(@PathVariable final String projectKey) {
 		return promotionService.getProjectPromotionStatus(projectKey);
 	}
 
 	@ApiOperation(value="Retrieve status information about the MAIN branch")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/main", method= RequestMethod.GET)
 	public AuthoringMain retrieveMain() throws BusinessServiceException {
 		return taskService.retrieveMain();
 	}
 
 	@ApiOperation(value="List Tasks within a Project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks", method= RequestMethod.GET)
 	public List<AuthoringTask> listTasks(@PathVariable final String projectKey) throws BusinessServiceException {
 		return taskService.listTasks(projectKey);
 	}
 
 	@ApiOperation(value="List authenticated user's Tasks across Projects")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/my-tasks", method= RequestMethod.GET)
 	public List<AuthoringTask> listMyTasks(@RequestParam(value = "excludePromoted", required = false) String excludePromoted) throws JiraException, BusinessServiceException {
 		return taskService.listMyTasks(ControllerHelper.getUsername(), excludePromoted);
 	}
 
 	@ApiOperation(value="List review tasks, with the current user or unassigned reviewer, across Projects")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/review-tasks", method= RequestMethod.GET)
 	public List<AuthoringTask> listMyOrUnassignedReviewTasks() throws JiraException, BusinessServiceException {
 		return taskService.listMyOrUnassignedReviewTasks();
 	}
 
 	@ApiOperation(value="Retrieve a Task within a Project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}", method= RequestMethod.GET)
 	public AuthoringTask retrieveTask(@PathVariable final String projectKey, @PathVariable final String taskKey) throws BusinessServiceException {
 		return taskService.retrieveTask(projectKey, taskKey);
 	}
 
 	@ApiOperation(value="Create a Task within a Project")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks", method= RequestMethod.POST)
 	public AuthoringTask createTask(@PathVariable final String projectKey, @RequestBody final AuthoringTaskCreateRequest taskCreateRequest) throws BusinessServiceException {
 		return taskService.createTask(projectKey, taskCreateRequest);
 	}
 
 	@ApiOperation(value = "Update a Task")
-	@ApiResponses({ @ApiResponse(code = 200, message = "OK") })
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value = "/projects/{projectKey}/tasks/{taskKey}", method = RequestMethod.PUT)
 	public AuthoringTask updateTask(@PathVariable final String projectKey, @PathVariable final String taskKey,  @RequestBody final AuthoringTaskUpdateRequest updatedTask) throws BusinessServiceException {
 		return taskService.updateTask(projectKey, taskKey, updatedTask);
 	}
 	
 	@ApiOperation(value = "Update a Project")
-	@ApiResponses({ @ApiResponse(code = 200, message = "OK") })
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value = "/projects/{projectKey}", method = RequestMethod.PUT)
 	public AuthoringProject updateProject(@PathVariable final String projectKey,  @RequestBody final AuthoringProject updatedProject) throws BusinessServiceException {
 		return taskService.updateProject(projectKey,  updatedProject);
 	}
 
 	@ApiOperation(value = "Retrieve Task Attachments")
-	@ApiResponses({ @ApiResponse(code = 200, message = "OK") })
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value = "/projects/{projectKey}/tasks/{taskKey}/attachments", method = RequestMethod.GET)
 	public List<TaskAttachment> getAttachmentsForTask(@PathVariable final String projectKey, @PathVariable final String taskKey) throws BusinessServiceException {
 		return taskService.getTaskAttachments(projectKey, taskKey);
 	}
 
 	@ApiOperation(value = "Leave comment for Task")
-	@ApiResponses({ @ApiResponse(code = 200, message = "OK") })
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value = "/projects/{projectKey}/tasks/{taskKey}/comment", method = RequestMethod.POST)
 	public void leaveComment(@PathVariable final String projectKey, @PathVariable final String taskKey, @RequestBody final String comment) throws BusinessServiceException {
 		taskService.leaveCommentForTask(projectKey, taskKey, comment);
 	}
 
 	@ApiOperation(value="Rebase an authoring Task")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}/rebase", method= RequestMethod.POST)
-	public ResponseEntity<String> rebaseTask(@PathVariable final String projectKey,
-											 @PathVariable final String taskKey) throws BusinessServiceException {
+	public ResponseEntity<String> rebaseTask(@PathVariable final String projectKey, @PathVariable final String taskKey) throws BusinessServiceException {
 		ProcessStatus  processStatus = rebaseService.getTaskRebaseStatus(projectKey, taskKey);
 		if (processStatus == null || !processStatus.getStatus().equals("Rebasing")) {
 			rebaseService.doTaskRebase(projectKey, taskKey);
@@ -216,19 +164,14 @@ public class ProjectController {
 	}
 	
 	@ApiOperation(value="Get rebase status of an authoring Task")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}/rebase/status", method= RequestMethod.GET)
-	public ProcessStatus getRebaseTaskStatus(@PathVariable final String projectKey,
-											 @PathVariable final String taskKey) throws BusinessServiceException {
+	public ProcessStatus getRebaseTaskStatus(@PathVariable final String projectKey, @PathVariable final String taskKey) {
 		return rebaseService.getTaskRebaseStatus(projectKey, taskKey);
 	}
 
 	@ApiOperation(value="Promote an authoring Task")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}/promote", method= RequestMethod.POST)
 	public ResponseEntity<String> promoteTask(@PathVariable final String projectKey,
 											  @PathVariable final String taskKey,
@@ -238,28 +181,17 @@ public class ProjectController {
 			promotionService.doTaskPromotion(projectKey, taskKey, mergeRequest);
 		}		
 		return new ResponseEntity<>(HttpStatus.OK);
-		/*Merge merge = branchService.mergeBranchSync(taskBranchPath, PathHelper.getParentPath(taskBranchPath), mergeRequest.getSourceReviewId());
-		if (merge.getStatus() == Merge.Status.COMPLETED) {
-			taskService.stateTransition(projectKey, taskKey, TaskStatus.PROMOTED);
-			notificationService.queueNotification(ControllerHelper.getUsername(),
-					new Notification(projectKey, taskKey, EntityType.Promotion, "Task successfully promoted"));
-		}
-		return getResponseEntity(merge);*/
 	}
 	
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}/promote/status", method= RequestMethod.GET)
-	public ProcessStatus getTaskPromotionStatus(@PathVariable final String projectKey,
-										   @PathVariable final String taskKey) throws BusinessServiceException {
+	public ProcessStatus getTaskPromotionStatus(@PathVariable final String projectKey, @PathVariable final String taskKey) {
 		return promotionService.getTaskPromotionStatus(projectKey, taskKey);
 	}
 	
 	@ApiOperation(value="Auto promote an authoring Task")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}/auto-promote", method= RequestMethod.POST)
-	public ResponseEntity<String> autoPromoteTask(@PathVariable final String projectKey,
-											  @PathVariable final String taskKey) throws BusinessServiceException {
+	public ResponseEntity<String> autoPromoteTask(@PathVariable final String projectKey, @PathVariable final String taskKey) throws BusinessServiceException {
 		ProcessStatus currentProcessStatus = promotionService.getAutomateTaskPromotionStatus(projectKey, taskKey);
 		if (!(null != currentProcessStatus && (currentProcessStatus.getStatus().equals("Rebasing") || currentProcessStatus.getStatus().equals("Classifying") || currentProcessStatus.getStatus().equals("Promoting")))) {
 			promotionService.queueAutomateTaskPromotion(projectKey, taskKey);
@@ -268,12 +200,9 @@ public class ProjectController {
 	}
 	
 	@ApiOperation(value="Get status of authoring task auto-promotion.")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "OK")
-	})
+	@ApiResponse(code = 200, message = "OK")
 	@RequestMapping(value="/projects/{projectKey}/tasks/{taskKey}/auto-promote/status", method= RequestMethod.GET)
-	public ProcessStatus getAutomateTaskPromotionStatus(@PathVariable final String projectKey,
-										   @PathVariable final String taskKey) throws BusinessServiceException {
+	public ProcessStatus getAutomateTaskPromotionStatus(@PathVariable final String projectKey, @PathVariable final String taskKey) {
 		return promotionService.getAutomateTaskPromotionStatus(projectKey, taskKey);
 	}
 
