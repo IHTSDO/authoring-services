28932b64e05dd1a914eb590c448d1de1e47eb627
Quyen Ly <quyen.ly@netcompany.com>
2018-12-24 17:23:06 +0700

    MAINT-14 Add condition checking

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/security/RequestHeaderAuthenticationDecorator.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/security/RequestHeaderAuthenticationDecorator.java
@@ -1,22 +1,23 @@
 package org.ihtsdo.snowowl.authoring.single.api.security;
 
-import com.google.common.base.Strings;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+import com.google.common.base.Strings;
 
 public class RequestHeaderAuthenticationDecorator extends org.ihtsdo.sso.integration.RequestHeaderAuthenticationDecorator {
 
@@ -52,11 +53,13 @@ public class RequestHeaderAuthenticationDecorator extends org.ihtsdo.sso.integra
 			token = overrideToken;
 			logger.warn("Using authentication override token"); // We don't log the token
 		}
-		List decoratedRoles = roles != null ? AuthorityUtils.commaSeparatedStringToAuthorityList(roles) : new ArrayList();
-		decoratedRoles.addAll(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
-		PreAuthenticatedAuthenticationToken decoratedAuthentication = new PreAuthenticatedAuthenticationToken(username, token, decoratedRoles);
-		decoratedAuthentication.setDetails(SecurityContextHolder.getContext().getAuthentication().getDetails());
-		SecurityContextHolder.getContext().setAuthentication(decoratedAuthentication);
+		if (!StringUtils.isEmpty(username) && !StringUtils.isEmpty(token)) {
+			List decoratedRoles = roles != null ? AuthorityUtils.commaSeparatedStringToAuthorityList(roles) : new ArrayList();
+			decoratedRoles.addAll(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+			PreAuthenticatedAuthenticationToken decoratedAuthentication = new PreAuthenticatedAuthenticationToken(username, token, decoratedRoles);
+			decoratedAuthentication.setDetails(SecurityContextHolder.getContext().getAuthentication().getDetails());
+			SecurityContextHolder.getContext().setAuthentication(decoratedAuthentication);
+		}
 		filterChain.doFilter(request, response);
 	}
 
