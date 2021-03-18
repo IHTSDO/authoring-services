daf8b7971e2a44559316fc1e8f328eb89d2ffbe8
Quyen Ly <quocquyenit@gmail.com>
2021-03-18 14:53:45 +0700

    MAINT-1559 Send notification to author of task which is going to re-assign to other author

--- src/main/java/org/ihtsdo/snowowl/authoring/single/api/pojo/EntityType.java
+++ src/main/java/org/ihtsdo/snowowl/authoring/single/api/pojo/EntityType.java
@@ -2,6 +2,6 @@ package org.ihtsdo.snowowl.authoring.single.api.pojo;
 
 public enum EntityType {
 
-	Validation, Feedback, Classification, BranchState, Rebase, Promotion
+	AuthorChange, Validation, Feedback, Classification, BranchState, Rebase, Promotion
 
 }
