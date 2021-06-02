0522d921ce0a8c9a1138902248d879058060ecaa
Quyen Ly <quocquyenit@gmail.com>
2021-06-02 15:26:10 +0700

    MAINT-1212 Update notification service to send the message right after it's in queue

--- src/main/java/org/ihtsdo/authoringservices/configuration/WebSocketConfiguration.java
+++ src/main/java/org/ihtsdo/authoringservices/configuration/WebSocketConfiguration.java
@@ -1,36 +1,21 @@
 package org.ihtsdo.authoringservices.configuration;
 
 import org.ihtsdo.authoringservices.service.NotificationService;
-import org.ihtsdo.authoringservices.service.monitor.MonitorService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.user.SimpUser;
-import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import java.util.Set;
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-	
-	@Autowired
-	private SimpUserRegistry simpUserRegistry;
-	
 	@Autowired
 	private NotificationService notificationService;
 
-	@Autowired
-	private MonitorService monitorService;
-	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry brokerRegistry) {
 		brokerRegistry.enableSimpleBroker("/topic");
@@ -46,17 +31,8 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 	}
 
 	@Scheduled(cron = "*/10 * * * * *")
-	public void readWebSocketConnections() {
-		if (logger.isDebugEnabled()) {
-			logger.debug("Current users: {}", simpUserRegistry.getUsers());
-		}
-		
-		Set<SimpUser> currentUsers = simpUserRegistry.getUsers();
-		for (SimpUser simpUser : currentUsers) {
-			String username =  simpUser.getName();
-			monitorService.keepMonitorsAlive(username);
-			notificationService.sendNotification(username);
-		}
+	public void autoSendingNotifications() {
+		notificationService.sendNotification();
 	}
 	
 }
--- src/main/java/org/ihtsdo/authoringservices/service/NotificationService.java
+++ src/main/java/org/ihtsdo/authoringservices/service/NotificationService.java
@@ -5,12 +5,17 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.ihtsdo.authoringservices.service.monitor.MonitorService;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.ihtsdo.authoringservices.domain.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpSubscription;
+import org.springframework.messaging.simp.user.SimpSubscriptionMatcher;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -25,31 +30,16 @@ public class NotificationService {
 	@Autowired
 	private SimpMessagingTemplate simpMessagingTemplate;
 
+	@Autowired
+	private SimpUserRegistry simpUserRegistry;
+
+	@Autowired
+	private MonitorService monitorService;
+
 	private final Map<String, List<Notification>> pendingNotifications = new HashMap<>();
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Deprecated
-	public List<Notification> retrieveNewNotifications(String username) {
-		if (pendingNotifications.containsKey(username)) {
-			synchronized (pendingNotifications) {
-				return pendingNotifications.remove(username);
-			}
-		}
-		return null;
-	}
-	
-	public void sendNotification(String username) {
-		if (pendingNotifications.containsKey(username)) {
-			synchronized (pendingNotifications) {
-				List<Notification> notifications = pendingNotifications.remove(username);
-				if (!CollectionUtils.isEmpty(notifications)) {
-					simpMessagingTemplate.convertAndSend("/topic/user/" + username + "/notifications", notifications.get(notifications.size() - 1));
-				}
-			}
-		}
-	}
-	
 	public Notification queueNotification(String username, Notification notification) {
 		final String projectKey = notification.getProject();
 		if (!Strings.isNullOrEmpty(projectKey)) {
@@ -65,9 +55,44 @@ public class NotificationService {
 				pendingNotifications.put(username, new ExpiringNotificationList(username));
 			}
 			pendingNotifications.get(username).add(notification);
+			sendNotification();
 		}
-		
+
 		return notification;
+	}
+
+	public void sendNotification() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Current users: {}", simpUserRegistry.getUsers());
+		}
+
+		Set<SimpUser> currentUsers = simpUserRegistry.getUsers();
+		for (SimpUser simpUser : currentUsers) {
+			String username =  simpUser.getName();
+			monitorService.keepMonitorsAlive(username);
+
+			SimpSubscriptionMatcher simpSubscriptionMatcher = new SimpSubscriptionMatcher() {
+				@Override
+				public boolean match(SimpSubscription subscription) {
+					return subscription.getDestination().equals("/topic/user/" + username + "/notifications");
+				}
+			};
+			Set<SimpSubscription> simpSubscriptions = simpUserRegistry.findSubscriptions(simpSubscriptionMatcher);
+			if (!simpSubscriptions.isEmpty()) {
+				sendNotification(username);
+			}
+		}
+	}
+	
+	public void sendNotification(String username) {
+		if (pendingNotifications.containsKey(username)) {
+			synchronized (pendingNotifications) {
+				List<Notification> notifications = pendingNotifications.remove(username);
+				if (!CollectionUtils.isEmpty(notifications)) {
+					simpMessagingTemplate.convertAndSend("/topic/user/" + username + "/notifications", notifications.get(notifications.size() - 1));
+				}
+			}
+		}
 	}
 
 	private final CacheBuilder<Notification, String> userNotificationCacheBuilder = CacheBuilder.newBuilder()
