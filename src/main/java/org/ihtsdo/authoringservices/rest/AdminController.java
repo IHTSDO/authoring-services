586baa2263361bec9e46166e598a9ccde87274b5
Quyen Ly <quocquyenit@gmail.com>
2024-08-16 13:25:21 +0700

    AMS-56 Add API that allows ADMIN to bulk-delete tasks

--- src/main/java/org/ihtsdo/authoringservices/rest/AdminController.java
+++ src/main/java/org/ihtsdo/authoringservices/rest/AdminController.java
@@ -1,6 +1,7 @@
 package org.ihtsdo.authoringservices.rest;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import net.rcarz.jiraclient.JiraException;
 import org.ihtsdo.authoringservices.domain.AuthoringProject;
@@ -11,10 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
 
 @RestController
 @Tag(name = "Admin")
@@ -29,9 +30,18 @@ public class AdminController {
 
     @Operation(summary = "Delete a given task key", description = "-")
     @DeleteMapping(value = "/projects/{projectKey}/tasks/{taskKey}")
-    public ResponseEntity<String> deleteTask(@PathVariable final String projectKey, @PathVariable final String taskKey) throws JiraException, BusinessServiceException {
+    public ResponseEntity<Void> deleteTask(@PathVariable final String projectKey, @PathVariable final String taskKey) throws JiraException, BusinessServiceException {
         AuthoringProject project = taskService.retrieveProject(projectKey, true);
-        adminService.deleteIssue(project.getCodeSystem(), taskKey);
+        adminService.deleteIssue(project, taskKey);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "Delete a given task key", description = "-")
+    @DeleteMapping(value = "/projects/{projectKey}/tasks/bulk-delete")
+    public ResponseEntity<Void> deleteTasks(@PathVariable final String projectKey,
+                                              @Parameter(description = "Task keys") @RequestParam final List<String> taskKeys) throws JiraException, BusinessServiceException {
+        AuthoringProject project = taskService.retrieveProject(projectKey, true);
+        adminService.deleteIssues(project, new HashSet<>(taskKeys));
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/org/ihtsdo/authoringservices/service/AdminService.java
+++ src/main/java/org/ihtsdo/authoringservices/service/AdminService.java
@@ -2,15 +2,17 @@ package org.ihtsdo.authoringservices.service;
 
 import net.rcarz.jiraclient.JiraClient;
 import net.rcarz.jiraclient.JiraException;
+import org.ihtsdo.authoringservices.domain.AuthoringProject;
 import org.ihtsdo.authoringservices.service.jira.ImpersonatingJiraClientFactory;
 import org.ihtsdo.authoringservices.service.jira.JiraHelper;
-import org.ihtsdo.otf.rest.client.terminologyserver.pojo.CodeSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class AdminService {
@@ -21,11 +23,20 @@ public class AdminService {
     @Qualifier("authoringTaskOAuthJiraClient")
     private ImpersonatingJiraClientFactory jiraClientFactory;
 
-    @PreAuthorize("hasPermission('ADMIN', 'global') || hasPermission('ADMIN', #codeSystem.branchPath)")
-    public boolean deleteIssue(CodeSystem codeSystem, String issueKey) throws JiraException {
-        logger.info("Deleting issue with key {} on code system {}", issueKey, codeSystem.getShortName());
+    @PreAuthorize("hasPermission('ADMIN', 'global') || hasPermission('ADMIN', #project.codeSystem.branchPath)")
+    public boolean deleteIssue(AuthoringProject project, String issueKey) throws JiraException {
+        logger.info("Deleting issue with key {} on project {}", issueKey, project.getKey());
         JiraClient adminJiraClient = jiraClientFactory.getAdminInstance();
         return JiraHelper.deleteIssue(adminJiraClient, issueKey);
+    }
+
+    @PreAuthorize("hasPermission('ADMIN', 'global') || hasPermission('ADMIN', #project.codeSystem.branchPath)")
+    public void deleteIssues(AuthoringProject project, Set<String> issueKeys) throws JiraException {
+        logger.info("Deleting issues [{}] on project {}", issueKeys, project.getKey());
+        JiraClient adminJiraClient = jiraClientFactory.getAdminInstance();
+        for (String issueKey : issueKeys) {
+            JiraHelper.deleteIssue(adminJiraClient, issueKey);
+        }
     }
 
 }
