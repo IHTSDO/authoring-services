befd9314a50e9987bf5ca8ad2e565f42d4991e56
Quyen Ly <quocquyenit@gmail.com>
2024-12-19 16:53:16 +0700

    AMS-12 Integrate the IMS search user API into authoirng-service

--- src/main/java/org/ihtsdo/authoringservices/domain/JiraUserGroup.java
+++ src/main/java/org/ihtsdo/authoringservices/domain/JiraUserGroup.java
@@ -31,26 +31,4 @@ public class JiraUserGroup {
 	public void setUsers(UserGroupItem users) {
 		this.users = users;
 	}
-
-	public class UserGroupItem {
-		private List <JiraUser> items;
-
-		private int size;
-
-		public List <JiraUser> getItems() {
-			return items;
-		}
-
-		public void setItems(List <JiraUser> items) {
-			this.items = items;
-		}
-
-		public int getSize() {
-			return size;
-		}
-
-		public void setSize(int size) {
-			this.size = size;
-		}
-	}
 }
--- src/main/java/org/ihtsdo/authoringservices/service/client/IMSClient.java
+++ src/main/java/org/ihtsdo/authoringservices/service/client/IMSClient.java
@@ -3,8 +3,11 @@ package org.ihtsdo.authoringservices.service.client;
 import org.ihtsdo.authoringservices.domain.User;
 import org.ihtsdo.otf.rest.client.ExpressiveErrorHandler;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +22,9 @@ public class IMSClient {
     private HttpHeaders headers;
 
     public static final String ROLE_PREFIX = "ROLE_";
+
+    private static final ParameterizedTypeReference<List<User>> USER_LIST_TYPE_REF = new ParameterizedTypeReference<>() {
+    };
 
     public IMSClient(String imsUrl, String authToken) {
         headers = new HttpHeaders();
@@ -52,5 +58,14 @@ public class IMSClient {
             user.setRoles(roles);
         }
         return user;
+    }
+
+    public List<User> searchUserByGroupname(String groupname, int startAt, int maxResults) {
+        Map<String, String> params = new HashMap<>();
+        params.put("groupname", groupname);
+        params.put("startAt", String.valueOf(startAt));
+        params.put("maxResults", String.valueOf(maxResults));
+        ResponseEntity<List<User>> responseEntity =  restTemplate.exchange("/group/user?groupname={groupname}&startAt={startAt}&maxResults={maxResults}", HttpMethod.GET, null, USER_LIST_TYPE_REF, params);
+        return responseEntity.getBody();
     }
 }
