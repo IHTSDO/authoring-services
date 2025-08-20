152b5b2d78ffb2072997ae0209301a8aa68bdf99
Quyen Ly <quocquyenit@gmail.com>
2025-08-20 16:08:43 +0700

    MAINT-2928 Remove new caching

--- src/main/java/org/ihtsdo/authoringservices/service/BranchServiceCache.java
+++ src/main/java/org/ihtsdo/authoringservices/service/BranchServiceCache.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,7 +20,8 @@ public class BranchServiceCache {
         this.snowstormRestClientFactory = snowstormRestClientFactory;
     }
 
-    @Cacheable(value = "branchCache", key = "#branchPath", sync = true)
+    // Temporary disable caching
+    // @Cacheable(value = "branchCache", key = "#branchPath", sync = true)
     public Branch getBranchOrNull(String branchPath) throws ServiceException {
         try {
             return toBranch(snowstormRestClientFactory.getClient().getBranch(branchPath));
