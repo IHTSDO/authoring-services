6a9753dd2696a819872dc003eab93e4b14df33e6
Elena Ilyukhina <eil@ihtsdo.org>
2025-10-24 16:07:55 +0100

    PIP-811 Standardise version endpoint

--- src/main/java/org/ihtsdo/authoringservices/rest/VersionController.java
+++ src/main/java/org/ihtsdo/authoringservices/rest/VersionController.java
@@ -2,38 +2,25 @@ package org.ihtsdo.authoringservices.rest;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.ihtsdo.authoringservices.domain.BuildVersion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Tag(name = "Version")
 @RestController
-@RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE })
+@Tag(name = "Version", description = "Build Version")
 public class VersionController {
 
 	@Autowired(required = false)
 	private BuildProperties buildProperties;
 
-	@RequestMapping(value = "/version", method = RequestMethod.GET)
-	@Operation(summary = "Returns version of current deployment", description = "Returns the software-build version from the package manifest." )
-	@ResponseBody
-	public Map<String, String> getVersion() {
-		Map<String, String> versionMap = new HashMap<>();
-		String version;
-		if (buildProperties != null) {
-			version = buildProperties.getVersion();
-		} else {
-			version = "Build with maven to get package version.";
-		}
-		versionMap.put("package_version", version);
-		return versionMap;
+	@Operation(summary = "Software build version and timestamp.")
+    @GetMapping(value = "/version", produces = "application/json")
+	public BuildVersion getBuildInformation() {
+        if (buildProperties == null) {
+            throw new IllegalStateException("Build properties are not present.");
+        }
+        return new BuildVersion(buildProperties.getVersion(), buildProperties.getTime().toString());
 	}
 
 }
